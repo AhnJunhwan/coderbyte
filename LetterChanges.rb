3647a9976c2da5990edbfb04f62fbe020b2634c2
AhnJunhwan <kinzzss123@gmail.com>
2015-07-31 17:22:52 +0900

    Create LetterChanges.rb

---
+++ LetterChanges.rb
@@ -0,0 +1,30 @@
+def LetterChanges(str)
+
+  # code goes here
+ 
+  return str.tr('a-y', 'b-z')
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterChanges(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
